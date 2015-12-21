75426e0d3878bf3e49a53f1b04c9301a370d904b
Shana L Moore <shana.lavina.moore@gmail.com>
2015-12-21 14:23:31 -0800

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,45 +23,47 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block
-  movie = Movie.create do |m|
-    m.title = "Woo!"
+  Movie.create do |m|
+    Movie.new
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  movie = Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  movie = Movie.last.title
 end
 
 def can_get_size_of_the_database
-  Movie.all.size
+  movie = Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
   Movie.find(1).title
 end
 
-def can_find_by_multiple_attributes
+def can_find_by_different_attributes
   # title == "Title"
   # release_date == 2000, 
   # director == "Me"
-  Movie.find_by(title: "Title", release_date: 2000)
+  Movie.find_by(title: "Title")
+  Movie.find_by(release_date: "2000")
+  Movie.find_by(director: "Me")
 end
 
-def can_find_using_where_clause
+def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where("release_date > 2002").order(release_date: :desc)
+   movie = Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
-  movie.title = "Even Awesomer Flick"
+  movie.update(title: "Even Awesomer Flick")
   movie.save
 end
 
@@ -77,7 +79,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.update_all(title: "A Movie")
+  Movie.update_all "title = 'A Movie'"
 end
 
 def can_destroy_a_single_item
