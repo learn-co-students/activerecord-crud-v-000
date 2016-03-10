32052ea4559570bf39e48d4ae570dc6eaf0b15ea
brianmenard <useyourbrian@gmail.com>
2016-03-10 14:15:35 +0000

    Done.

---
+++ db/migrate/02_change_release_date_datatype.rb
@@ -0,0 +1,7 @@
+class ChangeReleaseDateDatatype < ActiveRecord::Migration
+
+  def change
+    change_column :movies, :release_date, :integer
+  end
+  
+end
---
+++ db/migrate/03_change_in_theaters_datatype.rb
@@ -0,0 +1,7 @@
+class ChangeInTheatersDatatype < ActiveRecord::Migration
+  
+  def change
+    change_column :movies, :in_theaters, :boolean
+  end
+  
+end
