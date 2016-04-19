30d29a906d7ac12e5b8213249bb839daf9f0f9f2
rafeeq-123 <rafeeq-123@hotmail.com>
2016-04-19 11:25:53 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20160416234913) do
 
   create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "release_date"
-    t.string   "director"
-    t.string   "lead"
-    t.datetime "in_theaters"
+    t.string  "title"
+    t.integer "release_date"
+    t.string  "director"
+    t.string  "lead"
+    t.boolean "in_theaters"
   end
 
 end
