f85898286feda4ab20718f83e8e8f9b696741c94
aprietof <adrianprieto00@gmail.com>
2016-04-30 19:10:18 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,16 +61,16 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  m = find_by(title: "Awesome Flick")
+  m.update(title: "Even Awesomer Flick")
+  m.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  m = Movie.find_by(title: "Wat?")
+  m.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
@@ -78,18 +78,18 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all(title: "A Movie")
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  m = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  m.destroy
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all
 end
