88e0a8bb46159ee096b03178624920d09fd970bb
mcglaser <michaelcharlesglaser@gmail.com>
2016-05-07 18:13:23 +0000

    Done.

--- db/migrate/20160506234557_create_movies.rb
+++ db/migrate/20160506234557_create_movies.rb
@@ -1,9 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-     add_column :movies, :title, :string
-     add_column :movies, :release_date, :string
-     add_column :movies, :director, :string
-     add_column :movies, :lead, :string
-     add_column :movies, :in_theaters, :string
+    create_table :movies do |t|
+      t.string  :title
+      t.integer :release_date
+      t.string  :director
+      t.string  :lead
+      t.boolean :in_theaters
+    end
   end
 end
