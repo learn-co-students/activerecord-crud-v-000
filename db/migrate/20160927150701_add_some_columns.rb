0277925e91fdd10a0a185c9d5afff4a6495879ec
David Kennell <davidkennell0@gmail.com>
2016-09-27 17:03:14 +0000

    Finished activerecord-crud-v-000

---
+++ db/migrate/20160927150701_add_some_columns.rb
@@ -0,0 +1,9 @@
+class AddSomeColumns < ActiveRecord::Migration
+  def change
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :datetime
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
+  end
+end
