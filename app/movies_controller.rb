81aec41e445bd77aa037ab6bddf8822ec7cc3c9d
bharti gurnaney <bharti0717@yahoo.com>
2016-11-16 18:27:59 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,48 +19,58 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    __
+    m.title="Home Alone"
+    m.release_date=1990
   end
+
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+ Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+Movie.find_by id:1
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+ Movie.where("title =? and release_date=?
+    and director=?","Title",2000,"Me" )
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  f=Movie.find_by release_date: "2002"
+  f.order("release_date  desc")
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
+  Movie.transaction do
+  i = Movie.lock.find_by title: "Awesome Flick"
+  i.title = 'Even Awesomer Flick'
+  i.save!
+end
   __
   __
 end
@@ -92,11 +102,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
