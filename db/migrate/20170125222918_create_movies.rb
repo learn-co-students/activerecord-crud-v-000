6818e62e75c3a711012b018f37eec5b938ed9e7d
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-01-26 14:43:27 +0000

    Done.

---
+++ db/migrate/20170125222918_create_movies.rb
@@ -0,0 +1,10 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |m|
+      m.string :title
+      m.datetime :release_date
+      m.string :lead
+      m.string :in_theaters
+    end
+  end
+end
