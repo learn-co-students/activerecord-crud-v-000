736f0cc07a8fa96ffe646d891bbca8c1d1ef3fd6
mxdavis <md01@me.com>
2017-04-02 00:45:16 +0300

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,3 @@
 class Movie < ActiveRecord::Base
 
-  attr_accessor :release_date, :title, :director, :lead, :in_theatres
-
-  def initialize(opts = {})
-    @title = title
-    @release_date = release_date
-    @director = director
-  end
-
 end
--- db/migrate/01_create_movies.rb
+++ db/migrate/01_create_movies.rb
@@ -6,7 +6,7 @@ class CreateMovies < ActiveRecord::Migration
       t.datetime :release_date
       t.string :director
       t.string :lead
-      t.string :in_theaters
+      t.boolean :in_theaters
     end
   end
 
