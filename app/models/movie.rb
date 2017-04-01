d89184ad5b28c24657d7224986b4441afd25e77f
mxdavis <md01@me.com>
2017-04-01 22:38:01 +0300

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+
+  attr_accessor :release_date, :title, :director, :lead, :in_theatres
+
+  def initialize(opts = {})
+    @title = title
+    @release_date = release_date
+    @director = director
+  end
+
 end
