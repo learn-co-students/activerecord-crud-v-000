e6073c58011917163ed0d1286d661013c9f32f5e
Erika Hughes <ehughes@knights.ucf.edu>
2017-04-02 19:48:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170402175608) do
     t.datetime "release_date"
     t.string   "director"
     t.string   "lead"
-    t.string   "in_theaters"
+    t.boolean  "in_theaters"
   end
 
 end
