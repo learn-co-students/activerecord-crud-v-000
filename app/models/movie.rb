cdcfcde4529adecba53047bd7026bf05308b3e52
Alex Pepper <axpepper@gmail.com>
2017-04-10 21:53:28 -0700

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,2 @@
 class Movie < ActiveRecord::Base
-  def self.initialize
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS movies (
-      id INTEGER PRIMARY KEY,
-      title TEXT,
-      release_date INTEGER,
-      director TEXT,
-      lead TEXT,
-      in_theaters boolean
-    )
-    SQL
-
-    ActiveRecord::Base.connection.execute(sql)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410022404) do
+ActiveRecord::Schema.define(version: 20170411025357) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string  "title"
+    t.integer "release_date"
+    t.string  "director"
+    t.string  "lead"
+    t.boolean "in_theaters"
+  end
 
 end
