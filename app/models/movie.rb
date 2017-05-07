e06d1bb3e87091fd8ff5fea615c1a23144e1b502
Aditi Green <asaki444@gmail.com>
2017-05-07 20:45:38 +0000

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+  attr_accessor :title
+  def initialize(title:)
+    @title = title
+    self.save
+  end
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS movies (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  length INTEGER
+  )
+  SQL
+  ActiveRecord::Base.connection.execute(sql)
 end
