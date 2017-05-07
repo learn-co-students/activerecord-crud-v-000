4e229ca38bdd47b7e06bce00634993574ce76479
Aditi Green <asaki444@gmail.com>
2017-05-07 19:35:02 +0000

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.create_table
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT
+  )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+ end
 end
