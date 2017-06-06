20d446a7748bc839ce99e93b309f8cd3b7577bc4
Niki Lee <ew1415048nk@gmail.com>
2017-06-06 18:52:47 +0900

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  # attr_accessor :title, :release_date, :director, :lead, :in_theaters
+  # def initialize
+  # end
+  #
+  # def initialize(hash)
+  #
+  # end
+  #
+  # def in_theaters?
+  #   @in_theaters
+  # end
 end
