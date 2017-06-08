84b304c6bb1b8600768cbc9f8481d1e3e6ee433e
Will-Sauer <will@heueso.com>
2017-06-08 16:45:55 -0700

    finished

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,47 +30,51 @@ Movie.create do |m|
 end
 
 def can_get_the_first_item_in_the_database
-  __
+Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.first.title
 end
 
 def can_find_by_multiple_attributes
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
-  __
+  attributes = {
+                :title => "Title",
+                :release_date => 2000,
+                :director => "Me"
+}
+  
+  Movie.find_by(attributes)
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.find_by(:title => "Awesome Flick")
+  movie.title = "Even Awesomer Flick"
+  movie.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  change = Movie.find_by(:title => "Wat?")
+  change.title = "Wat, huh?"
+  change.save
 end
 
 def can_update_multiple_items_at_once
@@ -78,20 +82,20 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all "title = 'A Movie'"
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  Movie.find_by(:title => "That One Where the Guy Kicks Another Guy Once").destroy
+  
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.all.each {|movie| movie.destroy}
 end
 
 
