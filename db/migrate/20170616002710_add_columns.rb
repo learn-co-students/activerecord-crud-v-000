06531c2349f5025cd59fa4670400b7a3e91e44ce
Juan Gongora <gongora.animations@gmail.com>
2017-06-16 04:12:11 +0000

    Done.

---
+++ db/migrate/20170616002710_add_columns.rb
@@ -0,0 +1,9 @@
+class AddColumns < ActiveRecord::Migration
+  def change
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :integer
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
+  end
+end
