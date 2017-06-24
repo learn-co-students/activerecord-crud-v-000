e6bb3421fdcfd7de12586d43b80ebb2a1e381ff9
charlie lee <ctlee9096@gmail.com>
2017-06-24 21:34:20 +0000

    Done.

--- db/migrate/20170624205721_create_movies.rb
+++ db/migrate/20170624205721_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    create_table :movies do |t|
+    create_table :movie do |t|
     t.string :title
     t.integer :release_date
     t.string :director
     t.string :lead
-    t.boolean :in_theaters
+    t.string :in_theaters
     end
   end
 end
