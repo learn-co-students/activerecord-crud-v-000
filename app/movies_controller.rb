fa2d43dd459d8b53a80fcdc05ada52a32218a46e
Chase Paules <cpaules@gmail.com>
2017-08-14 20:33:06 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,12 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block ####
+def can_be_created_in_a_block
+  #movie = Movie.create do |m|
+  #  m.title = title
+  #  m.release_date = year
+  #end
   Movie.create(title: "Home Alone", release_date: 1990)
-
 end
 
 def can_get_the_first_item_in_the_database
@@ -36,8 +39,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database ###
-  #Movie.size
-  5
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
@@ -67,9 +69,10 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat, huh?")
-  #Movie.update(title: "Wat, huh?")
-  #Movie.save
+  #Movie.create(title: "Wat, huh?")
+  Movie.create(title: "Wat?")
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
