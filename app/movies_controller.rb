071f595d4b83454ead3852ecd97b3a173447b7af
Julianne Kim <juliannne@gmail.com>
2017-11-10 20:29:19 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,38 +38,40 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.count
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.where(movie_id = 1).first.title
+  Movie.find(1).title
+
+  # Movie.where(movie_id = 1).first.title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where(title: "Title", release_date: 2000, director: "Me").first
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where("release_date > 2002").order("release_date DESC")
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  movie_title = Movie.find_by title: "Awesome Flick"
-  movie_title.update(title: "Even Awesomer Flick")
-  movie_title.save
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.title = "Even Awesomer Flick"
+  movie.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie_title = Movie.find_by title: "Wat?"
+  movie_title = Movie.find_by(title: "Wat?")
   movie_title.update(title: "Wat, huh?")
 end
 
@@ -83,7 +85,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie_title = Movie.find_by title: "That One Where the Guy Kicks Another Guy Once"
+  movie_title = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie_title.destroy
 end
 
