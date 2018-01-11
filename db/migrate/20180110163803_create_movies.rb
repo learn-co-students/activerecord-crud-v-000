35b58e01227844e3062e118fd06183765c58a45d
Nolan Byrnes <nolanehs@gmail.com>
2018-01-11 07:47:23 +0000

    Done.

---
+++ db/migrate/20180110163803_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.text :title
+      t.datetime :release_date
+      t.text :director
+      t.text :lead
+      t.boolean :in_theaters
+
+      t.timestamps
+  end
+end
+end
