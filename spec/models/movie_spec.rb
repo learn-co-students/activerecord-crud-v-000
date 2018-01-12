49c5af86042e8d3b0fe933e38ef50a6a625808a7
churchwerks <steven.lennis@icloud.com>
2018-01-12 15:59:29 -0600

    Not working

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -13,6 +13,7 @@ describe 'Movie' do
 
   context 'Movie.new' do
     let(:movie) { Movie.new }
+    binding.pry
     it 'has a title' do
       movie.title = "The Matrix"
       expect(movie.title).to eq("The Matrix")
