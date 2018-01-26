5d44a711b2e2d7d86eb5d600e639e79f5ea8639e
rh24 <ms.rebeccahong@gmail.com>
2018-01-26 12:12:29 -0500

    Refactor solution movies_controller.rb

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,7 +42,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(1).title
+  Movie.find(1).title
 end
 
 def can_find_by_multiple_attributes
@@ -55,14 +55,16 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.find_by_sql("SELECT * FROM movies WHERE release_date > 2002 ORDER BY release_date DESC")
+  # Movie.find_by_sql("SELECT * FROM movies WHERE release_date > 2002 ORDER BY release_date DESC")
+
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
-  movie.update(title: "Even Awesomer Flick")
+  movie.title = "Even Awesomer Flick"
   movie.save
 end
 
@@ -82,7 +84,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.last
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.destroy
 end
 
