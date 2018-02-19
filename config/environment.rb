a9b6e49701513f2f3b9b21f76d5e8b7221645860
jljardon <github email address>
2018-02-19 01:14:27 -0500

    add creating, reading, updating and destroying methods

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,8 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+# Had to add this since the logger was in debug showing every SQL sentence everytime the test ran.
+# ActiveRecord::Base.logger.level = 1  # or Logger::INFO
+ActiveRecord::Base.logger = nil
 
 require_all 'app'
