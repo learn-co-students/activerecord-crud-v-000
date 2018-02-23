122330cf12ccdfce9914bcaa1eadfb928cb2ebdc
Katrina Brinson <kbrinso@gmail.com>
2018-02-23 19:41:08 +0000

    starting update_multiple

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,16 +78,15 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.create(title: "Wat?")
+  movie.update(title: "Wat, huh?")
+  # movie.save
 end
 
 def can_update_multiple_items_at_once
