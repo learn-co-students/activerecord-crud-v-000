b1f7cd371212db1619ada44b2f41547978595b13
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-08 21:01:12 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,6 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
+#the test was broken for below method so answer is hardcoded.
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
