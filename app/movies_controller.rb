641e863f7bfe87cebacc2fa3ccf74d487122a3e9
Sam. O <samrey2018@gmail.com>
2018-03-28 19:35:09 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,7 +53,8 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where( title: "Title", release_date: 2000, director: "Me")
+  Movie.where( title: "Title", release_date: 2000, director: "Me").first
+
 end
 
 def can_find_using_where_clause_and_be_sorted
@@ -75,27 +76,29 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title:"Wat?")
+  movie.update(title:"Wat, huh?")
+
 end
+
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all(title:"A Movie")
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  movie = Movie.find_by(title:"That One Where the Guy Kicks Another Guy Once")
+  movie.destroy
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all
 end
