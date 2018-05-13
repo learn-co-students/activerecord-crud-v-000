8441b273d6c65929c63b3e19cf9c811a93caaf19
Victoria Meng <nights1ststar@protonmail.com>
2018-05-13 22:14:56 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,8 +22,16 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-  movie = Movie.create(args)
+def can_be_created_in_a_block(args = nil)
+  if args
+    Movie.create(args)
+  else
+    Movie.create do |m|
+     m.release_date = 1990
+     m.title = "Home Alone"
+    end
+  end
+
 end
 
 def can_get_the_first_item_in_the_database
