7f63154a5aaaec69f556b68f0284ebbcab8f85af
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-04 18:30:22 +0000

    Done.

--- db/migrate/20180604162741_create_movies.rb
+++ db/migrate/20180604162741_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |col|
       col.string :title
-      col.date :release_date
+      col.datetime :release_date
       col.string :director
       col.string :lead
       col.boolean :in_theaters
