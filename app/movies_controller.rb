6e44d0933816067de39c242394469c949fc10684
Grace Shih <vocalise@gmail.com>
2018-06-09 00:04:34 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -91,12 +91,12 @@ end
 
 def can_destroy_a_single_item
   m = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  m.destroy
+  m.delete
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.destroy_all
+  Movie.delete_all
 end
