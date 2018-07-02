b934b86c1fbe6c9f7905215e20920cffd5b6edb3
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-02 02:41:34 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,12 +19,13 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = attributes.Movie.new
+  movie = Movie.create(attributes)
+
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    __
+    args
   end
 end
 
