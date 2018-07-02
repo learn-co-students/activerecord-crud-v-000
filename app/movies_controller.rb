ee16875b257f7a67e6e046726bc6365629f1d2c0
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-02 02:28:56 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,4 +1,4 @@
-# Replace the '__' in the below methods to to make the specs pass!
+require 'pry'# Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = attributes.Movie.new
 end
 
 def can_be_created_in_a_block(args = __)
@@ -52,7 +52,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -92,11 +92,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
