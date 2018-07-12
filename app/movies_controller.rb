b1c640a543cc4886e29eee210c8003a9e31d45d1
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 19:13:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,10 @@ end
 
 def can_be_created_in_a_block(args = nil)
   Movie.create do |movie|
-    args.each {|arg, value| movie.arg = value }
+    if args
+      args.each {|arg, value| movie.arg = value }
+    else
+  end
 end
 
 def can_get_the_first_item_in_the_database
