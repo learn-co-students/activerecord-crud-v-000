9f2d66829abe3ad88bad7ad0e9c2756b722c1c27
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 19:17:17 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,8 +26,7 @@ end
 def can_be_created_in_a_block(args = nil)
   Movie.create do |movie|
     if args
-      binding.pry
-      #args.each {|arg, value| movie.send("#{arg}="), "#{value}"}
+      args.each {|arg, value| movie.send("#{arg}="), "#{value}"}
     else
       movie.title = "Home Alone"
       movie.release_date = 1990
