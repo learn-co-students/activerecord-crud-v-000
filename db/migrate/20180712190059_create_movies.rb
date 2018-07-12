50e9ead4c4db460a64588b13249429716ff5bb9a
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 19:03:22 +0000

    automatically backed up by learn

--- db/migrate/20180712190059_create_movies.rb
+++ db/migrate/20180712190059_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      add_column :title, :string
-      add_column :release_date, :datetime
-      add_column :director, :string
-      add_column :lead, :string
-      add_column :in_theaters, :boolean
+      t.add_column :title, :string
+      t.add_column :release_date, :datetime
+      t.add_column :director, :string
+      t.add_column :lead, :string
+      t.add_column :in_theaters, :boolean
     end
   end
 end
