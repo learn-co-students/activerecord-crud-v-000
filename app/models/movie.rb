80313e285c649f1060da1c9e768605c0887951e6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-15 06:00:31 +0000

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  
+  # def initialize(attributes)
+  #   attributes.each do |key, value|
+  #     self.send("#{key}=", value)
+  #   end
+  # end
+  
+  # self.column_names.each do |c_name|
+  #   attr_accessor c_name.to_sym
+  # end
 end
