c3c8d7556c32e7bc8311ac139742e6dd39ebcfa8
Barbara Choy <barbchoy@gmail.com>
2018-10-10 03:06:56 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,32 +25,32 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title:"Home Alone",release_date:"1990" })
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = "1990"
+    m.title = args[:title]
+    m.release_date = args[:release_date]
     m.save
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by(id:1)
 end
 
 def can_find_by_multiple_attributes
@@ -58,7 +58,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(id: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
