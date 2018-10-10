6830a3767a394d6a4182cc7173cb2a6a8662ef9d
Barbara Choy <barbchoy@gmail.com>
2018-10-10 16:47:14 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = {title:"Home Alone",release_date:"1990" })
+def can_be_created_in_a_block(args = { title: "Home Alone", release_date: "1990" })
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
@@ -58,13 +58,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(id: "Title", release_date: 2000, director: "Me")
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where("release_date >= 2000").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
