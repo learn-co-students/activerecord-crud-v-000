a42a60a2ec41c007b45b3da94499f4675f180d51
Laura Messa Travin <laura.travin@gmail.com>
2018-11-20 00:20:35 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,7 +32,7 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
 
   Movie.create do |m|
-          if args.empty?
+          if args = nil
            m.title = "Home Alone"
            m.release_date = 1990
          else
