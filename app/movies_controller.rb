22e9c0e138b762c330a17988835ebb4aca39501a
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-07 06:03:23 +0000

    passed 3 tests today

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -4,11 +4,12 @@
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
+require 'pry'
 
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  movie
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,40 +20,48 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
-    __
+    # binding.pry
+    m.title = args[:title]
+    m.release_date = args[:release_date]
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  # binding.pry
+  # Movie.find{|m| m.title.first.title}
+  # Movie.find_by(title: "Movie_0")
+  # Movie.select(:title).first
+  Movie.limit(1).pluck(:title)[0]
+  # movie = Movie.first(:title)[0]
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie = Movie.pluck(:title).last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.limit(1).pluck(:title)[0]
+
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.pluck(:title, :release_date, :director)[0]
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -92,11 +101,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
