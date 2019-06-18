5be149bd693c95bdc21d5692874a35ef1aed6406
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 22:58:28 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,10 +27,10 @@ end
   # title == "Home Alone"
   # release_date == 1990
   
-def can_be_created_in_a_block (title = "Home Alone", release_date = 1990)
+def can_be_created_in_a_block (args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
-    m.title
-    m.release_date
+    m.title = args[:title]
+    m.release_date = args[:release_date]
   end
 end
 
