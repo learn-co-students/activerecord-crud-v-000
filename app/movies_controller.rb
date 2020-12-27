a0cd373201c5923e328d25bee87f180b4b9dba24
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-27 02:46:44 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,52 +1,63 @@
+
 # Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
+require 'pry'
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
   # Initialize movie and then and save it
-  attributes = {
+
+    attributes = {
       title: "The Sting",
       release_date: 1973,
       director: "George Roy Hill",
       lead: "Paul Newman",
       in_theaters: false
-  }
-  movie = __
+    }
+
+    movie = Movie.new(attributes)
+    movie.save
+    movie
+
+
 end
 
-def can_be_created_in_a_block(args = __)
+
+def can_be_created_in_a_block(title = "Home Alone", release_date = "1990")
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
-  Movie.create do |m|
-    __
+@title = title
+@release_date = release_date
+   Movie.create do |movie|
+    movie.title = title
+    movie.release_date = release_date
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  first = Movie.find[1]
+
 end
 
 def can_get_the_last_item_in_the_database
-  __
+
 end
 
 def can_get_size_of_the_database
-  __
+
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
 end
 
 def can_find_by_multiple_attributes
@@ -58,7 +69,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -83,7 +94,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+
 end
 
 def can_destroy_a_single_item
