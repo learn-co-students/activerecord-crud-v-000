91e16191bb7b6cfef79f9f392b01c1fa9696b048
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-15 00:23:42 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,11 +32,10 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
     m.title = args[:title]
     m.release_date = args[:release_date]
   end
-  # binding.pry
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
